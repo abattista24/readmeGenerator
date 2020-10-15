2f0b81b84612c3cb60acebddf9fc699cce2a1781
Anna Battistals <anna.r.battista@gmail.com>
2020-10-15 14:33:50 -0600

    added psydo code

--- Develop/index.js
+++ Develop/index.js
@@ -14,3 +14,32 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+
+// create a README
+  //ask user for information
+      //title
+      //description
+      //Installation
+      //Usage
+      //license
+          //give options
+      //contributing
+      //tests
+      //questions
+        //link to github
+        //link to email 
+          // use <a href= "mailto: email"> email </a>
+
+  // write user input to readme.md file, call userReadme.md
+    //needs to have table of contenets with usable links
+      //Installation
+      //Usage
+      //License
+      //contributing
+      //Tests
+      //questions
+        
+        
+        
