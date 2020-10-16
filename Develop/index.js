94c3dc2394349b8f65c5f9feb9c48741c1d2654b
Anna Battistals <anna.r.battista@gmail.com>
2020-10-15 23:27:45 -0600

    question array created

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,59 @@
+// create a README
+
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
+    const questions = [ 
+      //title
+      {type:"input",
+      message:"Enter a title for your readme.",
+      name:"title"
+      },
+      //description
+      {type:"input",
+      message: "Write a short description of the project",
+      name:"description"
+      },
+      //Installation
+      {type:"input",
+      message:"Please describe the installation required",
+      name:"install",
+      },
+      //Usage
+      {type:"input",
+      message:"How would a person use your application?",
+      name:"use",
+      },
+      //license
+      //give options
+      {type:"list",
+      message:"Please select the license type that best fits your application",
+      name:"license",
+      choices: ["type 1", "type 2", "type 3", "type 4"]
+      },
+      //contributing
+      {type:"input",
+      message:"What contributions can people make to your application",
+      name:"contribution",
+      },
+      //tests
+      {type:"input",
+      message:"How can a test be performed?",
+      name:"test",
+      },
+      //link to github
+      {type:"input",
+      message:"What is the URL for your GitHub repo for this application?",
+      name:"gitHubUrl",
+    },
+    //link to email 
+    // use <a href= "mailto: email"> email </a>
+      {type:"input",
+      message:"What is your mail address?",
+      name:"email",
+      }
+    ];
+      
+      // function to write README file
+      function writeToFile(fileName, data) {
 }
 
 // function to initialize program
@@ -17,20 +66,6 @@ init();
 
 
 
-// create a README
-  //ask user for information
-      //title
-      //description
-      //Installation
-      //Usage
-      //license
-          //give options
-      //contributing
-      //tests
-      //questions
-        //link to github
-        //link to email 
-          // use <a href= "mailto: email"> email </a>
 
   // write user input to readme.md file, call userReadme.md
     //needs to have table of contenets with usable links
